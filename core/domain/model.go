ad56b5ac831861414e7a878c5c0b60239cc3b191
captainthx <106069231+captainthx@users.noreply.github.com>
2024-08-24 11:29:33 +0700

    chore: update domain struct

--- core/domain/model.go
+++ core/domain/model.go
@@ -23,7 +23,7 @@ const (
 	Cancel  orderStatus = "cancel"
 )
 
-func (o *orderStatus) Sacn(value interface{}) error {
+func (o *orderStatus) Scan(value interface{}) error {
 	*o = orderStatus(value.([]byte))
 	return nil
 }
@@ -32,7 +32,7 @@ func (o orderStatus) Value() (interface{}, error) {
 	return string(o), nil
 }
 
-func (r *Role) Sacn(value interface{}) error {
+func (r *Role) Scan(value interface{}) error {
 	*r = Role(value.([]byte))
 	return nil
 }
@@ -44,20 +44,30 @@ func (r Role) Value() (interface{}, error) {
 // all the domain models are defined here
 type User struct {
 	gorm.Model
-	Name     string `json:"name" gorm:"type:varchar(30);not null"`
-	Username string `json:"username" gorm:"type:varchar(20);not null;unique"`
-	Password string `json:"password" gorm:"type:varchar(255);not null"`
-	Avatar   string `json:"avatar"`
-	Email    string `json:"email" gorm:"type:varchar(50);not null;unique"`
-	Role     Role   `json:"role" gorm:"type:ENUM('admin', 'customer');default:'customer'"`
-	Orders   []Order
+	Name     string  `json:"name" gorm:"type:varchar(30);not null"`
+	Username string  `json:"username" gorm:"type:varchar(20);not null;unique"`
+	Password string  `json:"password" gorm:"type:varchar(255);not null"`
+	Avatar   string  `json:"avatar"`
+	Email    string  `json:"email" gorm:"type:varchar(50);not null;unique"`
+	Role     Role    `json:"role" gorm:"type:ENUM('admin', 'customer');default:'customer'"`
+	Orders   []Order `gorm:"foreignKey:UserID"`
+	Cart     Cart    `gorm:"foreignKey:UserID"`
+}
+
+type Category struct {
+	gorm.Model
+	Name     string    `json:"name" gorm:"type:varchar(100);not null"`
+	Products []Product `gorm:"foreignKey:CategoryID"`
 }
 
 type Product struct {
 	gorm.Model
-	Name     string  `json:"name" gorm:"type:varchar(100);not null"`
-	Price    float64 `json:"amount" gorm:"type:decimal(7,2); not null"`
-	Quantity int     `json:"quantity" gorm:"not null"`
+	Name         string         `json:"name" gorm:"type:varchar(100);not null"`
+	Price        float64        `json:"amount" gorm:"type:decimal(7,2); not null"`
+	Quantity     int            `json:"quantity" gorm:"not null"`
+	CategoryID   uint           `json:"category_id"`
+	ProductImage []ProductImage `gorm:"foreignKey:ProductID;constraint:OnDelete:CASCADE;"`
+	CartID       uint           `json:"cart_id"`
 }
 
 type ProductImage struct {
@@ -66,17 +76,29 @@ type ProductImage struct {
 	URL       string `json:"url" gorm:"type:varchar(255);not null"`
 }
 
+type Cart struct {
+	gorm.Model
+	UserID    uint       `json:"user_id"`
+	CartItems []CartItem `gorm:"foreignKey:CartID"`
+}
+type CartItem struct {
+	gorm.Model
+	UserID    uint `json:"user_id"`
+	CartID    uint `json:"cart_id"`
+	ProductID uint `json:"product_id"`
+	Quantity  int  `json:"quantity"`
+}
+
 type Order struct {
-	ID            uuid.UUID   `json:"id" grom:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	Status        orderStatus `json:"status" gorm:"type:ENUM('pending', 'success', 'cancel');default:'pending'"`
-	TotalPay      float64     `json:"total_pay" gorm:"type:decimal(7,2);"`
-	UserID        uint        `json:"user_id"`
-	ProductId     uint        `json:"product_id"`
-	TransactionId uint        `json:"transaction_id"`
-	CreateAt      time.Time   `json:"create_at"`
-	UpdateAt      time.Time   `json:"update_at"`
-	OrderItems    []OrderItem
-	Transaction   Transaction
+	gorm.Model
+	OrderID     uuid.UUID   `json:"order_id" gorm:"type:char(36);unique"`
+	Status      orderStatus `json:"status" gorm:"type:ENUM('pending', 'success', 'cancel');default:'pending'"`
+	TotalPay    float64     `json:"total_pay" gorm:"type:decimal(7,2);"`
+	UserID      uint        `json:"user_id"`
+	CreatedAt   time.Time   `json:"created_at"`
+	UpdatedAt   time.Time   `json:"updated_at"`
+	OrderItems  []OrderItem `gorm:"foreignKey:OrderID"`
+	Transaction Transaction `gorm:"foreignKey:OrderID"`
 }
 
 type OrderItem struct {
@@ -89,7 +111,7 @@ type OrderItem struct {
 
 type Transaction struct {
 	gorm.Model
-	OrderID uint      `json:"order_id"`
+	OrderID uuid.UUID `json:"order_id" `
 	Amount  float64   `json:"amount" gorm:"type:decimal(7,2);"`
 	Status  string    `json:"status"`
 	PayTime time.Time `json:"pay_time"`
